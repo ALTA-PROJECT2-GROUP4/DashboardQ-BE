e0570e33fcd80f6ecc9ee6f40280bd815aca34e0
alfianadityads <alfianadityadwi@gmail.com>
2023-03-06 21:55:02 +0700

    chore: build user core entity

--- features/users/UserEntity.go
+++ features/users/UserEntity.go
@@ -1,1 +1,18 @@
 package users
+
+import (
+	// "github.com/labstack/echo/v4"
+)
+
+type Core struct {
+	ID        uint
+	Name      string
+	DateBirth string
+	Email     string `validate:"omitempty,email"`
+	Password  string `validate:"min=6,emitempty"`
+	Gender    string
+	Role      string
+	Team      string
+	Status    string
+	Phone     string
+}
