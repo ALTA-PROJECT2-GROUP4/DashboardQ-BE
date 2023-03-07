39b22e6116f0e541f2a1e57110fe368b925262f3
alfianadityads <alfianadityadwi@gmail.com>
2023-03-07 18:32:10 +0700

    chore: make function update data from admin side

--- features/users/data/query.go
+++ features/users/data/query.go
@@ -138,7 +138,37 @@ func (uq *userQuery) Update(userID uint, newUpdate users.Core) (users.Core, erro
 }
 
 // UpdateAdm implements users.UserData
-func (*userQuery) UpdateAdm(adminID uint, userID uint, newUpdate users.Core) (users.Core, error) {
-	panic("unimplemented")
+func (uq *userQuery) UpdateAdm(adminID uint, userID uint, newUpdate users.Core) (users.Core, error) {
+	if adminID != 1 {
+		log.Println("admin only")
+		return users.Core{}, errors.New("access denied")
+	}
+	if userID == 1 {
+		log.Println("access denied")
+		return users.Core{}, errors.New("access denied")
+	}
+	if newUpdate.Email != "" {
+		dupEmail := User{}
+		err := uq.db.Where("email = ?", newUpdate.Email).First(&dupEmail).Error
+		if err == nil {
+			log.Println("duplicated")
+			return users.Core{}, errors.New("email duplicated")
+		}
+	}
+	cnv := CoreToModel(newUpdate)
+	qry := uq.db.Model(&User{}).Where("id = ?", userID).Updates(&cnv)
+	affrows := qry.RowsAffected
+	if affrows == 0 {
+		log.Println("no rows affected")
+		return users.Core{}, errors.New("no data updated")
+	}
+	err := qry.Error
+	if err != nil {
+		log.Println("update user query error", err.Error())
+		return users.Core{}, errors.New("user not found")
+	}
+	result := ModelToCore(cnv)
+	result.ID = userID
+	return result, nil
 }
 
