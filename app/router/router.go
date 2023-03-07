ced4ea1ab77d1dddf51294dff859585f171bda3f
alfianadityads <alfianadityadwi@gmail.com>
2023-03-07 22:36:53 +0700

    chore: add all user endpoint route

--- app/router/router.go
+++ app/router/router.go
@@ -1,13 +1,13 @@
 package router
 
 import (
-	// "dashboardq-be/app/config"
+	"dashboardq-be/app/config"
 	userData "dashboardq-be/features/users/data"
 	userHdl "dashboardq-be/features/users/handler"
 	userServ "dashboardq-be/features/users/services"
 
 	"github.com/labstack/echo/v4"
-	// "github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/echo/v4/middleware"
 	"gorm.io/gorm"
 )
 
@@ -16,7 +16,16 @@ func InitRouter(db *gorm.DB, e *echo.Echo) {
 	userService := userServ.New(userData)
 	userHandler := userHdl.New(userService)
 
-	e.POST("/register", userHandler.Register())
+	// LOGIN
 	e.POST("/login", userHandler.Login())
-	// e.PUT("/users", userHandler.Update(), middleware.JWT([]byte(config.JWTKey)))
+	// ADMIN
+	e.POST("/register", userHandler.Register(), middleware.JWT([]byte(config.JWTKey)))
+	e.GET("/users", userHandler.ProfileAdm(), middleware.JWT([]byte(config.JWTKey)))
+	e.GET("/users", userHandler.ShowAllAdm(), middleware.JWT([]byte(config.JWTKey)))
+	e.PUT("/users/:user_id", userHandler.UpdateAdm(), middleware.JWT([]byte(config.JWTKey)))
+	e.DELETE("/users/:user_id", userHandler.Deactive(), middleware.JWT([]byte(config.JWTKey)))
+	// USER
+	e.GET("/profile", userHandler.ShowAll(), middleware.JWT([]byte(config.JWTKey)))
+	e.GET("/profile/:user_id", userHandler.Profile(), middleware.JWT([]byte(config.JWTKey)))
+	e.PUT("/profile/:user_id", userHandler.Update(), middleware.JWT([]byte(config.JWTKey)))
 }
