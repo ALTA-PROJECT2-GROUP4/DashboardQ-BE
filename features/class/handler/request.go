529f21fac563c64836bfbb9f9bb03b3dee0ab601
yuda <adiyudapranata@gmail.com>
2023-03-08 18:14:48 +0700

    feat: build class handler request

---
+++ features/class/handler/request.go
@@ -0,0 +1,27 @@
+package handler
+
+import "dashboardq-be/features/class"
+
+type CreateReq struct {
+	Name       string `json:"name" form:"name"`
+	StartClass string `json:"start_class" form:"start_class"`
+	EndClass   string `json:"end_class" form:"end_class"`
+	IdUser     uint   `json:"id_user" form:"id_user"`
+}
+
+func CrtToCore(data interface{}) *class.Core {
+	res := class.Core{}
+
+	switch data.(type) {
+	case CreateReq:
+		cln := data.(CreateReq)
+		res.Name = cln.Name
+		res.StartClass = cln.StartClass
+		res.EndClass = cln.EndClass
+		res.IdUser = cln.IdUser
+	default:
+		return nil
+	}
+
+	return &res
+}
