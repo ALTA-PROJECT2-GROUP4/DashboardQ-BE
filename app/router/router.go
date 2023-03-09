3cc417a6b9bddaf6824f88bb52abafe1789cdea4
alfianadityads <alfianadityadwi@gmail.com>
2023-03-09 18:55:48 +0700

    chore: make route endpoint for mentee post and ge all mentee data

--- app/router/router.go
+++ app/router/router.go
@@ -10,6 +10,10 @@ import (
 	classHndl "dashboardq-be/features/class/handler"
 	classServ "dashboardq-be/features/class/services"
 
+	menteeData "dashboardq-be/features/mentee/data"
+	menteeHdl "dashboardq-be/features/mentee/handler"
+	menteeServ "dashboardq-be/features/mentee/services"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"gorm.io/gorm"
@@ -24,16 +28,26 @@ func InitRouter(db *gorm.DB, e *echo.Echo) {
 	classService := classServ.New(classData)
 	classHandler := classHndl.New(classService)
 
+	menteeData := menteeData.New(db)
+	menteeService := menteeServ.New(menteeData)
+	menteeHandler := menteeHdl.New(menteeService)
+
 	// LOGIN
 	e.POST("/login", userHandler.Login())
+
 	// ADMIN
 	e.POST("/register", userHandler.Register(), middleware.JWT([]byte(config.JWTKey)))
 	e.GET("/users", userHandler.ShowAll(), middleware.JWT([]byte(config.JWTKey)))
 	e.PUT("/users/:user_id", userHandler.UpdateAdm(), middleware.JWT([]byte(config.JWTKey)))
 	e.DELETE("/users/:user_id", userHandler.Deactive(), middleware.JWT([]byte(config.JWTKey)))
+
 	// USER
 	e.GET("/profile/:user_id", userHandler.Profile(), middleware.JWT([]byte(config.JWTKey)))
 	e.PUT("/profile", userHandler.Update(), middleware.JWT([]byte(config.JWTKey)))
+
+	// MENTEE
+	e.POST("/mentee", menteeHandler.AddMentee(), middleware.JWT([]byte(config.JWTKey)))
+	e.GET("/mentee", menteeHandler.ShowAll(), middleware.JWT([]byte(config.JWTKey)))
 
 	// CLASS
 	e.POST("/create", classHandler.Create(), middleware.JWT([]byte(config.JWTKey)))
