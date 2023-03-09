78e95f71b572abaf1cec8d439c86482170b23c24
alfianadityads <alfianadityadwi@gmail.com>
2023-03-09 15:14:07 +0700

    chore: build mentee entity and all interface

---
+++ features/mentee/MenteeEntity.go
@@ -0,0 +1,42 @@
+package mentee
+
+import "github.com/labstack/echo/v4"
+
+type Core struct {
+	Name            string
+	Phone           string
+	Telegram        string
+	Email           string
+	Class           string
+	Gender          string
+	Address         string
+	HomeAddress     string
+	DateBirth       string
+	Status          string
+	EmergencyName   string
+	EmergencyPhone  string
+	EmergencyStatus string
+	Category        string
+	Major           string
+	Graduate        string
+}
+
+type ClassCore struct {
+	ID   uint
+	Name string
+}
+
+type MenteeHandler interface {
+	AddMentee() echo.HandlerFunc
+	ShowAll() echo.HandlerFunc
+}
+
+type MenteeService interface {
+	AddMentee(token interface{}, newMentee Core) (Core, error)
+	ShowAll() ([]Core, error)
+}
+
+type MenteeData interface {
+	AddMentee(userID uint, newMentee Core) (Core, error)
+	ShowAll() ([]Core, error)
+}
